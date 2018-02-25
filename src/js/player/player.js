65c6fc95d91c10ea07a522ebfbfcfd02919ec63a
fatlard1993 <fatlard1993@gmail.com>
2018-02-24 22:32:53 -0800

    add wakelock support, make changes for new socket stuff, fix no one playing detection

--- src/js/player/player.js
+++ src/js/player/player.js
@@ -1,10 +1,14 @@
-/* global Cjs, Dom, Log, Socket, Interact */
+/* global Cjs, Dom, Log, WS, Interact, LogHelp */
 
 var Loaded = false;
 
 function Load(){
 	if(Loaded) return;
 	Loaded = true;
+
+	var wakelock;
+
+	if(navigator.getWakeLock) wakelock = navigator.getWakeLock('screen');
 
 	var x;
 
@@ -49,7 +53,7 @@ function Load(){
 			var playersList = Dom.createElem('ul', { id: 'PlayersList' });
 
 			Dom.Content.appendChild(lobbyButton);
-			Dom.Content.appendChild(startButton);
+			if(!Player.ready) Dom.Content.appendChild(startButton);
 			Dom.Content.appendChild(playersList);
 
 			drawPlayersList();
@@ -110,7 +114,7 @@ function Load(){
 			var submissionList = Dom.createElem('ul', { id: 'SubmissionList' });
 			var submissionCount = submissions.length;
 
-			if(!submissionCount){
+			if(submissionCount <= 1){
 				var lobbyButton = Dom.createElem('button', { id: 'LobbyButton', textContent: 'Back to Lobby' });
 
 				currentBlackHeading.textContent = 'No one is playing here';
@@ -142,6 +146,10 @@ function Load(){
 			Dom.Content.appendChild(submissionList);
 		},
 		vote_results: function(submissions){
+			Player.ready = 0;
+			Player.submission = 0;
+			Player.placedVote = 0;
+
 			var currentBlackHeading = Dom.createElem('div', { id: 'CurrentBlackHeading', innerHTML: Game.currentBlack });
 
 			var submissionList = Dom.createElem('ul', { id: 'SubmissionList' });
@@ -166,6 +174,10 @@ function Load(){
 			Dom.Content.appendChild(scoresButton);
 			if(!Game.winner) Dom.Content.appendChild(playAgainButton);
 			Dom.Content.appendChild(lobbyButton);
+
+			if(wakelock) wakelock.cancel();
+
+			screen.keepAwake = 0;
 		},
 		scores: function(){
 			var scoresList = Dom.createElem('ul', { id: 'ScoresList' });
@@ -187,18 +199,238 @@ function Load(){
 		}
 	};
 
-	function onSocketMessage(data){
-		Log()(data);
+	function updateGameTimer(timeLeft){
+		var gameTimer = document.getElementById('GameTimer');
 
-		if(data.command === 'challenge'){
-			Socket.active.send('{ "command": "challenge_response", "room": "player", "game_room": "'+ Player.room +'" }');
+		if(!gameTimer) return;
+
+		gameTimer.textContent = timeLeft +' Seconds Remaining';
+
+		if(timeLeft === 0) return;
+
+		setTimeout(function(){
+			updateGameTimer(timeLeft - 1);
+		}, 1000);
+	}
+
+	function drawPlayersList(){
+		var playersList = document.getElementById('PlayersList');
+		var playerCount = Game.players.length;
+
+		if(!playersList) return;
+
+		Dom.empty(playersList);
+
+		for(x = 0; x < playerCount; ++x){
+			var playerNameText = Game.players[x];
+			if(Game.readyPlayers.includes(Game.players[x])) playerNameText += ' READY';
+			var player_li = Dom.createElem('li', { className: 'player'+ (Game.players[x] === Player.name ? ' disabled' : ''), textContent: playerNameText });
+
+				playersList[Game.players[x] === Player.name ? 'insertBefore' : 'appendChild'](player_li, Game.players[x] === Player.name && playersList.children.length ? playersList.children[0] : null);
+		}
+	}
+
+	function drawWhitesList(){
+		var whitesList = document.getElementById('WhitesList');
+		var whiteCount = Game.currentWhites.length;
+
+		if(!whitesList) return;
+
+		Dom.empty(whitesList);
+
+		for(x = 0; x < whiteCount; ++x){
+			var li = Dom.createElem('li', { className: 'white', innerHTML: Game.currentWhites[x] });
+			li.setAttribute('data-text', Game.currentWhites[x]);
+
+			whitesList.appendChild(li);
+		}
+	}
+
+	function joinGame(){
+		if(!document.querySelectorAll('.invalid').length){
+			var nameInput = document.getElementById('JoinGameName');
+
+			if(Game.players.includes(nameInput.value)){
+				nameInput.className = nameInput.className.replace(/\svalidated|\sinvalid/g, '') +' invalid';
+
+				return;
+			}
+
+			Dom.Content = Dom.Content || document.getElementById('Content');
+
+			Player.name = nameInput.value;
+
+			Dom.cookie.set('player_name', Player.name);
+
+			WS.send({ command: 'player_join', game_room: Player.room, playerName: Player.name });
+
+			Dom.empty(Dom.Content);
+		}
+	}
+
+	function enterSubmission(){
+		if(!document.querySelectorAll('.invalid').length){
+			var submission = document.getElementById('SubmissionEntry').value;
+
+			Player.submission = submission;
+
+			var submissionCommand = {
+				command: 'player_enter_submission',
+				submission: submission
+			};
+
+			if(submission !== Player.usedWhite) submissionCommand.customWhite = true;
+
+			WS.send(submissionCommand);
+
+			if(Player.usedWhite.length){
+				WS.send({ command: 'player_use_white', text: Player.usedWhite });
+				Player.usedWhite = '';
+			}
+
+			Dom.draw('waiting_room');
+		}
+	}
+
+	function voteOnSubmission(submission){
+		Player.placedVote = 1;
+
+		WS.send({ command: 'player_place_vote', vote: submission }); //.replace(/"/gm, '\\"')
+
+		Dom.draw('waiting_room');
+	}
+
+	Dom.draw = function draw(view){
+		Dom.Content = Dom.Content || document.getElementById('Content');
+
+		Dom.empty(Dom.Content);
+
+		Dom.setTitle('humanity - player');
+
+		Game.currentView = view || 'main';
+
+		views[view || 'main'](arguments[1]);
+	};
+
+	Interact.onPointerUp = function(evt){
+		Log()(evt);
+
+		if(evt.target.id === 'JoinGameButton'){
+			evt.preventDefault();
+
+			joinGame();
 		}
 
-		else if(data.command === 'challenge_accept'){
+		else if(evt.target.id === 'GameStartButton'){
+			evt.preventDefault();
+
+			if(wakelock) wakelock.createRequest();
+
+			screen.keepAwake = 1;
+
+			Dom.remove(evt.target);
+
+			Player.ready = 1;
+
+			WS.send({ command: 'player_ready_to_play' });
+		}
+
+		else if(evt.target.id === 'ScoresButton'){
+			evt.preventDefault();
+
+			Dom.draw('scores');
+		}
+
+		else if(evt.target.id === 'PlayAgainButton'){
+			evt.preventDefault();
+
+			Dom.Content = Dom.Content || document.getElementById('Content');
+
+			Dom.empty(Dom.Content);
+
+			WS.send({ command: 'player_play_again' }, 1);
+		}
+
+		else if(evt.target.id === 'LobbyButton'){
+			evt.preventDefault();
+
+			Dom.Content = Dom.Content || document.getElementById('Content');
+
+			Dom.empty(Dom.Content);
+
+			window.location = window.location.protocol +'//'+ window.location.hostname +':'+ window.location.port +'/lobby';
+		}
+
+		else if(evt.target.id === 'EnterSubmission'){
+			evt.preventDefault();
+
+			enterSubmission();
+		}
+
+		else if(evt.target.className === 'white'){
+			evt.preventDefault();
+
+			var submissionInput = document.getElementById('SubmissionEntry');
+			var whiteText = evt.target.getAttribute('data-text');
+
+			submissionInput.value = whiteText;
+			Dom.validate(submissionInput);
+
+			Player.usedWhite = whiteText;
+		}
+
+		else if(evt.target.className === 'submission'){
+			evt.preventDefault();
+
+			voteOnSubmission(evt.target.getAttribute('data-text'));
+		}
+
+		else if(evt.target.id === 'EmptySubmissionEntry'){
+			evt.preventDefault();
+
+			document.getElementById('SubmissionEntry').value = '';
+			Dom.validate(document.getElementById('SubmissionEntry'));
+
+			Player.usedWhite = '';
+		}
+	};
+
+	Interact.onKeyUp = function(evt, keyPressed){
+		if(keyPressed === 'ENTER'){
+			if(document.getElementById('JoinGameButton')){
+				evt.preventDefault();
+
+				joinGame();
+			}
+
+			else if(document.getElementById('EnterSubmission')){
+				evt.preventDefault();
+
+				enterSubmission();
+			}
+		}
+
+		else if(evt.target.id === 'SubmissionEntry' && keyPressed === 'BACK_SPACE' && evt.target.value.length < 5){
+			Player.usedWhite = '';
+		}
+	};
+
+	WS.onmessage = function(data){
+		if(data.command === 'challenge_accept'){
 			Game.players = data.players;
 			Game.readyPlayers = data.readyPlayers;
 
-			Dom.draw();
+			if(WS.reconnecting){
+				WS.reconnecting = false;
+
+				if(WS.disconnectedQueue.length) WS.flushQueue();
+
+				else if(['start_screen', 'enter_submission', 'waiting_room', 'vote'].includes(Game.currentView)) WS.send({ command: 'player_join', game_room: Player.room, playerName: Player.name });
+
+				// else if(Game.currentView !== 'main') Dom.draw();
+			}
+
+			else Dom.draw();
 		}
 
 		else if(data.command === 'player_join_accept'){
@@ -208,9 +440,17 @@ function Load(){
 			Game.readyPlayers = data.readyPlayers;
 			Game.options = data.options;
 
-			if(data.state === 'voting') Dom.draw('vote', data.submissions);
+			if(data.state === 'voting'){
+				if(Player.placedVote) Dom.draw('waiting_room');
 
-			else if(data.state === 'entering_submissions') Dom.draw('enter_submission');
+				else Dom.draw('vote', data.submissions);
+			}
+
+			else if(data.state === 'entering_submissions'){
+				if(Player.submission) Dom.draw('waiting_room');
+
+				else if(Game.currentView !== 'enter_submission') Dom.draw('enter_submission');
+			}
 
 			else Dom.draw('start_screen');
 		}
@@ -287,217 +527,11 @@ function Load(){
 				waitingOnPlayersList.appendChild(li);
 			}
 		}
-	}
-
-	function updateGameTimer(timeLeft){
-		var gameTimer = document.getElementById('GameTimer');
-
-		if(!gameTimer) return;
-
-		gameTimer.textContent = timeLeft +' Seconds Remaining';
-
-		if(timeLeft === 0) return;
-
-		setTimeout(function(){
-			updateGameTimer(timeLeft - 1);
-		}, 1000);
-	}
-
-	function drawPlayersList(){
-		var playersList = document.getElementById('PlayersList');
-		var playerCount = Game.players.length;
-
-		if(!playersList) return;
-
-		Dom.empty(playersList);
-
-		for(x = 0; x < playerCount; ++x){
-			var playerNameText = Game.players[x];
-			if(Game.readyPlayers.includes(Game.players[x])) playerNameText += ' READY';
-			var player_li = Dom.createElem('li', { className: 'player'+ (Game.players[x] === Player.name ? ' disabled' : ''), textContent: playerNameText });
-
-				playersList[Game.players[x] === Player.name ? 'insertBefore' : 'appendChild'](player_li, Game.players[x] === Player.name && playersList.children.length ? playersList.children[0] : null);
-		}
-	}
-
-	function drawWhitesList(){
-		var whitesList = document.getElementById('WhitesList');
-		var whiteCount = Game.currentWhites.length;
-
-		if(!whitesList) return;
-
-		Dom.empty(whitesList);
-
-		for(x = 0; x < whiteCount; ++x){
-			var li = Dom.createElem('li', { className: 'white', innerHTML: Game.currentWhites[x] });
-			li.setAttribute('data-text', Game.currentWhites[x]);
-
-			whitesList.appendChild(li);
-		}
-	}
-
-	function joinGame(){
-		if(!document.querySelectorAll('.invalid').length){
-			var nameInput = document.getElementById('JoinGameName');
-
-			if(Game.players.includes(nameInput.value)){
-				nameInput.className = nameInput.className.replace(/\svalidated|\sinvalid/g, '') + ' invalid';
-
-				return;
-			}
-
-			Dom.Content = Dom.Content || document.getElementById('Content');
-
-			Player.name = nameInput.value;
-
-			Dom.cookie.set('player_name', Player.name);
-
-			Socket.active.send('{ "command": "player_join", "game_room": "'+ Player.room +'", "playerName": "'+ Player.name +'" }');
-
-			Dom.empty(Dom.Content);
-		}
-	}
-
-	function enterSubmission(){
-		if(!document.querySelectorAll('.invalid').length){
-			var submission = document.getElementById('SubmissionEntry').value;
-
-			Player.submission = submission;
-
-			var submissionCommand = {
-				command: 'player_enter_submission',
-				submission: submission
-			};
-
-			if(submission !== Player.usedWhite) submissionCommand.customWhite = true;
-
-			if(Player.usedWhite.length){
-				Socket.active.send(JSON.stringify({ command: 'player_use_white', text: Player.usedWhite }));
-				Player.usedWhite = '';
-			}
-
-			Socket.active.send(JSON.stringify(submissionCommand));
-
-			Dom.draw('waiting_room');
-		}
-	}
-
-	function voteOnSubmission(submission){
-		Socket.active.send('{ "command": "player_place_vote", "vote": "'+ submission.replace(/"/gm, '\\"') +'" }');
-
-		Dom.draw('waiting_room');
-	}
-
-	Dom.draw = function draw(view){
-		Dom.Content = Dom.Content || document.getElementById('Content');
-
-		Dom.empty(Dom.Content);
-
-		Dom.setTitle('humanity - player');
-
-		Game.currentView = view || 'main';
-
-		views[view || 'main'](arguments[1]);
 	};
 
-	Interact.onPointerUp = function(evt){
-		Log()(evt);
+	WS.room = 'player_'+ Player.room;
 
-		if(evt.target.id === 'JoinGameButton'){
-			evt.preventDefault();
-
-			joinGame();
-		}
-
-		else if(evt.target.id === 'GameStartButton'){
-			evt.preventDefault();
-
-			Dom.remove(evt.target);
-
-			Socket.active.send('{ "command": "player_ready_to_play" }');
-		}
-
-		else if(evt.target.id === 'ScoresButton'){
-			evt.preventDefault();
-
-			Dom.draw('scores');
-		}
-
-		else if(evt.target.id === 'PlayAgainButton'){
-			evt.preventDefault();
-
-			Dom.Content = Dom.Content || document.getElementById('Content');
-
-			Dom.empty(Dom.Content);
-
-			Socket.active.send('{ "command": "player_play_again" }');
-		}
-
-		else if(evt.target.id === 'LobbyButton'){
-			evt.preventDefault();
-
-			Dom.Content = Dom.Content || document.getElementById('Content');
-
-			Dom.empty(Dom.Content);
-
-			window.location = window.location.protocol +'//'+ window.location.hostname +':'+ window.location.port +'/lobby';
-		}
-
-		else if(evt.target.id === 'EnterSubmission'){
-			evt.preventDefault();
-
-			enterSubmission();
-		}
-
-		else if(evt.target.className === 'white'){
-			evt.preventDefault();
-
-			var submissionInput = document.getElementById('SubmissionEntry');
-			var whiteText = evt.target.getAttribute('data-text');
-
-			submissionInput.value = whiteText;
-			Dom.validate(submissionInput);
-
-			Player.usedWhite = whiteText;
-		}
-
-		else if(evt.target.className === 'submission'){
-			evt.preventDefault();
-
-			voteOnSubmission(evt.target.getAttribute('data-text'));
-		}
-
-		else if(evt.target.id === 'EmptySubmissionEntry'){
-			evt.preventDefault();
-
-			document.getElementById('SubmissionEntry').value = '';
-			Dom.validate(document.getElementById('SubmissionEntry'));
-
-			Player.usedWhite = '';
-		}
-	};
-
-	Interact.onKeyUp = function(evt, keyPressed){
-		if(keyPressed === 'ENTER'){
-			if(document.getElementById('JoinGameButton')){
-				evt.preventDefault();
-
-				joinGame();
-			}
-
-			else if(document.getElementById('EnterSubmission')){
-				evt.preventDefault();
-
-				enterSubmission();
-			}
-		}
-
-		else if(evt.target.id === 'SubmissionEntry' && keyPressed === 'BACK_SPACE' && evt.target.value.length < 5){
-			Player.usedWhite = '';
-		}
-	};
-
-	Socket.init(null, onSocketMessage);
+	WS.connect();
 }
 
 document.addEventListener('DOMContentLoaded', Load);
