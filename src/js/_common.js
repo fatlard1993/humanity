4b6fd76002ed984c7ac5eca6b625edb68cfddea6
fatlard1993 <fatlard1993@gmail.com>
2018-03-12 16:52:39 -0700

    update

--- src/js/_common.js
+++ src/js/_common.js
@@ -1,12 +1,10 @@
-/* global Log */
-
 var Cjs = {
 	clone: function clone(it){
-		if(typeof it !== 'object') return Log.error()('common', 'Cjs.clone only accepts Objects and Arrays');
+		if(typeof it !== 'object') return console.error('common', 'Cjs.clone only accepts Objects and Arrays');
 
 		if(it instanceof Array) return it.slice(0);
 
-		Log.warn(1)('common', 'Cjs.clone uses JSON stringify => parse to clone an object, some things may not behave as expected: Dates, functions, Infinity, undefined, etc..');
+		// console.warn('common', 'Cjs.clone uses JSON stringify => parse to clone an object, some things may not behave as expected: Dates, functions, Infinity, undefined, etc..');
 
 		return JSON.parse(JSON.stringify(it));
 	},
@@ -88,9 +86,6 @@ var Cjs = {
 
 		return result;
 	},
-	arrIsNegative: function arrIsNegative(arr){
-		return arr.every(function(element, index, array){ return element < 0; });
-	},
 	sortArrAlphaNumeric: function(arr){
 		var alphabeticRegex = /[^a-zA-Z]/g;
 		var numericRegex = /[^0-9]/g;
@@ -130,6 +125,77 @@ var Cjs = {
 
 		return arr.sort(sortAlphaNum);
 	},
+	arrIsNegative: function arrIsNegative(arr){
+		return arr.every(function(element, index, array){ return element < 0; });
+	},
+	IPToDec: function IPToDec(ip){
+		var octets = ip.split('.');
+		return ((((((+octets[0]) * 256) + (+octets[1])) * 256) + (+octets[2])) * 256) + (+octets[3]);
+	},
+	decToIP: function decToIP(int){
+		var oct_3 = int & 255;
+		var oct_2 = ((int >> 8) & 255);
+		var oct_1 = ((int >> 16) & 255);
+		var oct_0 = ((int >> 24) & 255);
+
+		return oct_0 + '.' + oct_1 + '.' + oct_2 + '.' + oct_3;
+	},
+	decToHex: function decToHex(dec, length){
+		length = length || 8;
+		var output = (parseInt(dec) + Math.pow(16, length)).toString(16).substr(-length).toString();
+
+		if(output === 'NaN'){
+			output = '0'.repeat(length);
+			console.error('dec2hex isNaN', dec, length, output);
+		}
+
+		return output;
+	},
+	stringToHex: function stringToHex(string, charBytes){
+		var hex = '', stringLen = string.length, x, charHex;
+
+		for(x = 0; x < stringLen; ++x){
+			charHex = Number(string.charCodeAt(x)).toString(16);
+			hex += charBytes ? '0'.repeat(charBytes - charHex.length) + charHex : charHex;
+		}
+
+		return hex;
+	},
+	hexToString: function hexToString(hex, charBytes){
+		hex = hex.toString();
+		charBytes = charBytes || 2;
+
+		var string = '', hexLen = hex.length, x;
+
+		for(x = 0; x < hexLen; x += charBytes){
+			string += String.fromCharCode(parseInt(hex.substr(x, charBytes), 16));
+		}
+
+		return string;
+	},
+	findRanges: function(arr, simplify){
+		var arrLength = arr.length;
+		if(!arrLength) return arr;
+
+		arr = arr.sort(function(a, b){ return a - b; });
+
+		var ranges = [], range = 0;
+
+		for(var x = 0; x < arrLength; ++x){
+			ranges[range] = ranges[range] || [];
+
+			if(simplify){
+				ranges[range][Math.min(1, ranges[range].length)] = arr[x];
+			}
+			else{
+				ranges[range].push(arr[x]);
+			}
+
+			if(arr[x + 1] - arr[x] !== 1 && x < arrLength - 1) range++;
+		}
+
+		return ranges;
+	},
 	rand: function rand(min, max){
 		return Math.random() * (max - min) + min;
 	},
@@ -141,6 +207,11 @@ var Cjs = {
 	},
 	randColor: function randColor(){
 		return '#'+ Math.floor(Math.random() * 16777215).toString(16);
+	},
+	randFromArr: function randFromArr(arr){
+		var arrLen = arr.length;
+
+		return arr[Cjs.randInt(0, arrLen)];
 	},
 	incChar: function incChar(char, inc){
 		return String.fromCharCode(char.charCodeAt(0) + (inc === 0 ? 0 : inc || 1));
