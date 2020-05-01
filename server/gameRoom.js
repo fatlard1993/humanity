690b25a0eea49b501cae52a770b6e5172e3f98a8
fatlard1993 <fatlard1993>
2020-05-01 15:19:48 -0700

    slpit rooms

--- server/gameRoom.js
+++ server/gameRoom.js
@@ -2,37 +2,6 @@ const log = require('log');
 const util = require('js-util');
 const Room = require('byod-game-engine/server/room');
 
-class LobbyRoom extends Room {
-	constructor(options, game){
-		super(options, game);
-
-		return this;
-	}
-
-	sendUpdate(){
-		var x = 0, room, rooms = {}, roomNames = Object.keys(this.game.rooms), count = roomNames.length;
-
-		for(; x < count; ++x){
-			if(roomNames[x] === 'lobby') continue;
-
-			room = this.game.rooms[roomNames[x]];
-
-			rooms[roomNames[x]] = {
-				players: 0,
-				options: room.options
-			};
-
-			for(var y = 0, yCount = room.playerNames.length; y < yCount; ++y){
-				if(room.players[room.playerNames[y]].type === 'play' && room.players[room.playerNames[y]].state !== 'inactive') ++rooms[roomNames[x]].players;
-			}
-		}
-
-		this.state.rooms = rooms;
-
-		this.broadcast('state', { rooms });
-	}
-}
-
 class GameRoom extends Room {
 	constructor(options, game){
 		super(options, game);
@@ -357,7 +326,4 @@ class GameRoom extends Room {
 	}
 }
 
-module.exports = {
-	GameRoom,
-	LobbyRoom
-};
+module.exports = GameRoom;
---
+++ server/lobbyRoom.js
@@ -0,0 +1,48 @@
+const log = require('log');
+const util = require('js-util');
+const Room = require('byod-game-engine/server/room');
+
+class LobbyRoom extends Room {
+	constructor(options, game){
+		super(options, game);
+
+		return this;
+	}
+
+	addPlayer(player){
+		super.addPlayer({
+			name: player.name,
+			state: player.state,
+			socket: player
+		});
+
+		this.sendUpdate();
+	}
+
+	sendUpdate(){
+		var x = 0, room, rooms = {}, roomNames = Object.keys(this.game.rooms), count = roomNames.length;
+
+		for(; x < count; ++x){
+			if(roomNames[x] === 'lobby') continue;
+
+			room = this.game.rooms[roomNames[x]];
+
+			rooms[roomNames[x]] = {
+				players: 0,
+				options: room.options
+			};
+
+			for(var y = 0, yCount = room.playerNames.length; y < yCount; ++y){
+				if(room.players[room.playerNames[y]].type === 'play' && room.players[room.playerNames[y]].state !== 'inactive') ++rooms[roomNames[x]].players;
+			}
+		}
+
+		log.error('XXXXXXX');
+
+		this.state.rooms = rooms;
+
+		this.broadcast('state', { rooms });
+	}
+}
+
+module.exports = 	LobbyRoom;
